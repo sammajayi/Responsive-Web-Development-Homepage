a9f1abe94564d505b1e3e7f85570beeba46fdd4f
Samuel Ajayi <samuelajayi554@gmail.com>
2024-10-30 06:12:53 -0700

    add client-side validation

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 const form = document.getElementById('myForm')
 const submitButton = document.getElementById('submitButton');
-let firstName = document.getElementById('fname');
-let lastName = document.getElementById('lname');
-let genderSelection = document.getElementById('dropdown');
-let weight = document.getElementById('weight');
-let height = document.getElementById('height');
+
+const firstName = document.getElementById('fname');
+const lastName = document.getElementById('lname');
+const gender = document.getElementById('gender-dropdown');
+const DOB = document.getElementById('date-of-birth');
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const activity = document.getElementById('activity-dropdown');
+const password = document.getElementById('password');
+
+
+
 
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+const fnameError = document.getElementById('fname-error');
+const lnameError = document.getElementById('lname-error');
+const genderError = document.getElementById('gender-error');
+const dobError = document.getElementById('dob-error');
+const weightError = document.getElementById('weight-error');
+const heightError = document.getElementById('height-error');
+const activityError = document.getElementById('activity-error');
+const pwdError = document.getElementById('pwd-error');
+
+
     if (firstName.value.trim() === ""){
-        alert("Please enter your first name");
-    }
-    if (lastName.value.trim() === ""){
-        alert("Please enter your last name");
-    }
-    if (genderSelection.value.trim() === ""){
-        alert("Select your gender")
+        fnameError.style.display = "block";
     } 
+    if (lastName.value.trim() === ""){
+       lnameError.style.display = "block";
+    }
+    if (gender.value.trim() === ""){
+        genderError.style.display = "block";
+    } 
+    if (DOB.value.trim() === ""){
+        dobError.style.display = "block";
+    }
+    if (weight.value.trim() === ""){
+        weightError.style.display = "block";
+    }
+    if (height.value.trim() === ""){
+        heightError.style.display = "block";
+    }
+    if (activity.value.trim() === ""){
+        activityError.style.display = "block";
+    }
+    if (password.value.trim() === ""){
+        pwdError.style.display = "block";
+    }
+
+    
 });
 
 
