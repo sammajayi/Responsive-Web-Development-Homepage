1e4bf9457d81ffb1e29f3b28a3f7abffd400663e
Samuel Ajayi <samuelajayi554@gmail.com>
2024-10-31 15:47:25 -0700

    updated the validation conditions

--- script.js
+++ script.js
@@ -29,24 +29,38 @@ const pwdError = document.getElementById('pwd-error');
 
     if (firstName.value.trim() === ""){
         fnameError.style.display = "block";
-    } 
+    } else {
+        fnameError.style.display = "none";
+    }
     if (lastName.value.trim() === ""){
        lnameError.style.display = "block";
+    } else {
+        lnameError.style.display = "none";
     }
     if (gender.value.trim() === ""){
         genderError.style.display = "block";
-    } 
+    } else {
+        genderError.style.display = "none";
+    }
     if (DOB.value.trim() === ""){
         dobError.style.display = "block";
+    } else {
+        dobError.style.display = "none";
     }
     if (weight.value.trim() === ""){
         weightError.style.display = "block";
+    } else {
+        weightError.style.display = "none";
     }
     if (height.value.trim() === ""){
         heightError.style.display = "block";
+    } else {
+        heightError.style.display = "none";
     }
     if (activity.value.trim() === ""){
         activityError.style.display = "block";
+    } else {
+        activityError.style.display = "none";
     }
     if (password.value.trim() === ""){
         pwdError.style.display = "block";
@@ -58,14 +72,14 @@ const pwdError = document.getElementById('pwd-error');
         pwdError.textContent = "Password must contain at least one number.";
     } else if (!password.value.match(/[^a-zA-Z\d]/)) {
         pwdError.textContent = "Password must contain at least one special character.";
-    } else {
+    } else if (password.value.length < 8) {
+        pwdError.textContent = "Password must be at least 8 characters";
+    }
+    else {
         pwdError.textContent = ""; // Clear error if all conditions are met
     }
 
-    // if (password > 8 && ) {
-    //     pwdError.textContent = "Password must be at least 8 characters";
-    // }
-
+ 
     
     
 });
